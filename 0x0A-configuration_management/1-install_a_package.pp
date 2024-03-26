52d134faa42468cda0f8cb827d9188bec82a1762
MutabPato <patrickmtbz@gmail.com>
2024-03-26 15:01:06 -0700

    package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,12 +3,10 @@ class { 'python3': }
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Class['python3'],
 }
 package { 'Werkzeug':
   ensure   => '2.1.1',
   provider => 'pip3',
-  require  => Class['python3'],
 }
 exec { 'python-installed':
   command => '/usr/bin/which python3',
