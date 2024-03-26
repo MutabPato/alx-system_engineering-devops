4a6a66040f953a1cc915c0cc96dd38f766554bf0
MutabPato <patrickmtbz@gmail.com>
2024-03-26 14:56:02 -0700

    puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,6 +4,10 @@ package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
 }
+package { 'Werkzeug':
+  ensure   => '2.1.1'
+  provider => 'pip3'
+}
 exec { 'python-installed':
   command => '/usr/bin/which python3',
 }
