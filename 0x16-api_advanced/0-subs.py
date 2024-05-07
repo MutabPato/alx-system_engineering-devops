9b75b8074f77e58c850dca94954d881cbffecc0a
MutabPato <patrickmtbz@gmail.com>
2024-05-07 11:36:43 -0700

    Subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,12 +11,12 @@ def number_of_subscribers(subreddit):
     """
     queries the Reddit API and returns the number of subscribers
     """
-    url =f"https://www.reddit.com/dev/api/r/{subreddit}/about.json"
+    url = f"https://www.reddit.com/dev/api/r/{subreddit}/about.json"
 
-    headers = {'User-Agent': 'Pato/1.0'} 
+    headers = {'User-Agent': 'Pato/1.0'}
 
     response = requests.get(url, headers=headers)
-    
+
     if response.status_code == 200:
         about = response.json()
         return about['data']['subscribers']
